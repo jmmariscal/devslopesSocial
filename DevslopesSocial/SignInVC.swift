2c181be0bf386581c70b0dee6ac804d59d7c1fd6
Juan M Mariscal <juanmmariscal@Juans-MacBook-Pro.local>
2017-01-12 19:59:28 -0800

    Sign in layout complete

--- DevslopesSocial/SignInVC.swift
+++ DevslopesSocial/SignInVC.swift
@@ -9,10 +9,15 @@
 import UIKit
 
 class SignInVC: UIViewController {
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +25,9 @@ class SignInVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    
+    
 
 }
 
