170154a1396849873a393056c78b6c86a47df9c1
Juan M Mariscal <juanmmariscal@Juans-MacBook-Pro.local>
2017-01-13 16:37:05 -0800

    Sign in ui custom classes complete

---
+++ DevslopesSocial/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  DevslopesSocial
+//
+//  Created by Juan M Mariscal on 1/13/17.
+//  Copyright © 2017 Juan M Mariscal. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0
---
+++ DevslopesSocial/FancyTextField.swift
@@ -0,0 +1,32 @@
+//
+//  FancyTextField.swift
+//  DevslopesSocial
+//
+//  Created by Juan M Mariscal on 1/13/17.
+//  Copyright © 2017 Juan M Mariscal. All rights reserved.
+//
+
+import UIKit
+
+class FancyTextField: UITextField {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        layer.borderColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.2).cgColor
+        layer.borderWidth = 1.0
+        layer.cornerRadius = 5.0
+        
+    }
+    
+    override func textRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.insetBy(dx: 10, dy: 5)
+    }
+    
+    override func editingRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.insetBy(dx: 20, dy: 5)
+    }
+    
+    
+
+}
