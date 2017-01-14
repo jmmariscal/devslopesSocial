df4060764ffc50fe2ac97b69a9004a58217292ad
Juan M Mariscal <juanmmariscal@Juans-MacBook-Pro.local>
2017-01-13 19:40:11 -0800

    added Facebook SignIn Authentication

--- DevslopesSocial/SignInVC.swift
+++ DevslopesSocial/SignInVC.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
+import Firebase
 
 class SignInVC: UIViewController {
     
@@ -25,8 +28,39 @@ class SignInVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func facebookBtnTapped(_ sender: AnyObject) {
+        
+        let facebookLogin = FBSDKLoginManager()
+        
+        facebookLogin.logIn(withReadPermissions: ["email"], from: self) { (result, error) in
+            if error != nil {
+                
+                print("TEST: unable to authenticate with Facebook - \(error)")
+            } else if result?.isCancelled == true {
+                
+                print("TEST: User cancelled Facebook authentication")
+            } else {
+                
+                print("TEST: Successfully authenticated with Facebook")
+                let credential = FIRFacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
+                self.firebaseAuth(credential)
+            }
+        }
+    }
     
-    
+    func firebaseAuth(_ credential: FIRAuthCredential) {
+        
+        FIRAuth.auth()?.signIn(with: credential, completion: { (user, error) in
+            
+            if error != nil {
+                
+                print("TEST: Unable to authenticate with Firebase - \(error)")
+            } else {
+                
+                print("TEST: Succesfully auth with Firebase")
+            }
+        })
+    }
     
 
 }
