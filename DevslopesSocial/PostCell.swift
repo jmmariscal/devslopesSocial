dd568d956f350c932444f797bd194aec6fee161a
Juan M Mariscal <juanmmariscal@Juans-MacBook-Pro.local>
2017-01-15 21:17:38 -0800

    TableView UI completed

---
+++ DevslopesSocial/PostCell.swift
@@ -0,0 +1,29 @@
+//
+//  PostCell.swift
+//  DevslopesSocial
+//
+//  Created by Juan M Mariscal on 1/15/17.
+//  Copyright © 2017 Juan M Mariscal. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    @IBOutlet weak var profileImg: UIImageView!
+    @IBOutlet weak var userNameLbl: UILabel!
+    @IBOutlet weak var postImg: UIImageView!
+    @IBOutlet weak var caption: UITextView!
+    @IBOutlet weak var likesLbl: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    
+    
+    
+    
+
+}
