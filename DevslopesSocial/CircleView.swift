29e11f0b5fe9ad818268c439f6e65b89eb30f937
Juan M Mariscal <juanmmariscal@Juans-MacBook-Pro.local>
2017-01-15 20:54:59 -0800

    Feed UI pt.1 complete

--- DevslopesSocial/CircleView.swift
+++ DevslopesSocial/CircleView.swift
@@ -1,24 +1,29 @@
 //
-//  FancyView.swift
+//  CircleView.swift
 //  DevslopesSocial
 //
-//  Created by Juan M Mariscal on 1/13/17.
+//  Created by Juan M Mariscal on 1/15/17.
 //  Copyright © 2017 Juan M Mariscal. All rights reserved.
 //
 
 import UIKit
 
-class FancyView: UIView {
-
+class CircleView: UIImageView {
+    
     override func awakeFromNib() {
+        
         super.awakeFromNib()
         
         layer.shadowColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.6).cgColor
         layer.shadowOpacity = 0.8
         layer.shadowRadius = 5.0
         layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
-        layer.cornerRadius = 2.0
-        
     }
-    
+
+    override func layoutSubviews() {
+        
+        super.layoutSubviews()
+        
+        layer.cornerRadius = self.frame.width / 2
+    }
 }
