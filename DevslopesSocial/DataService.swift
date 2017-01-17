666b1a27d4648667fc3d1a7e2930dabb39e0f38a
Juan M Mariscal <juanmmariscal@Juans-MacBook-Pro.local>
2017-01-16 20:43:50 -0800

    Completed Firebase data

--- DevslopesSocial/DataService.swift
+++ DevslopesSocial/DataService.swift
@@ -20,6 +20,7 @@ class DataService {
     private var _REF_POSTS = DB_BASE.child("posts")
     private var _REF_USERS = DB_BASE.child("users")
     
+    
     var REF_BASE: FIRDatabaseReference {
         return _REF_BASE
     }
@@ -36,6 +37,8 @@ class DataService {
         
         REF_USERS.child(uid).updateChildValues(userData)
     }
+    
+    
     
     
     
